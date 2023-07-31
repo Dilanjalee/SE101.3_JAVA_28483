89cfa08757b165a02418b3758c70eb82b7eee6d4
Dilanjalee S Rajapaksha <136807659+DilanjaleeSRajapaksha@users.noreply.github.com>
2023-07-31 17:15:53 +0530

    Add files via upload

---
+++ Lab01_20230504/MyInfo/src/main/java/com/mycompany/myinfo/MyInfo.java
@@ -0,0 +1,14 @@
+package com.mycompany.myinfo;
+public class MyInfo
+{
+
+    public static void main(String[] args)
+    {
+        String name = "Dilanjalee";
+        String degreeProgram = "Software Engineering";
+
+        // Display the information on separate lines
+        System.out.println("My Name is: "+name);
+        System.out.println("My Degree Program: "+degreeProgram);
+    }
+}
